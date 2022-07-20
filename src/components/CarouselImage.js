e26cb234984627e99b537feb2259878c5f7b12bc
benjaminluan <benjaminluan1@gmail.com>
2022-07-20 18:46:47 -0400

    Finished Landing

--- src/components/CarouselImage.js
+++ src/components/CarouselImage.js
@@ -1,16 +1,6 @@
-
-
-
-
-
 export const Images = [
   {
     id: 1,
-    title: "1:1 Treact Template",
     img: "./images/treact.png",
-    source: "https://github.com/benjaminluan/Treact-Project",
-    preview: "https://benjaminluan.github.io/Treact-Project/",
-    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
-    language: "HTML, CSS ",
   },
 ];
