cbd866508a7cfce89174a4bb2e2745f205441de0
benjaminluan <benjaminluan1@gmail.com>
2022-07-20 09:06:10 -0400

    First Commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Landing.css";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="carousel">
+        <div className="carousel__slides"></div>
+        <div className="carousel__indicator"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
