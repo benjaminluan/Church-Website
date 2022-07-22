8ab1db912a2be65710b3d38a87d811a9f160f908
benjaminluan <benjaminluan1@gmail.com>
2022-07-22 08:15:23 -0400

    Install react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
